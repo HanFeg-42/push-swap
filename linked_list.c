f78427e0f12d26f2824755b2461631505314b09a
HanFeg-42 <hananefegrachi2002@gmail.com>
2025-01-19 01:18:02 +0100

    linked_list 5 functions

---
+++ linked_list.c
@@ -0,0 +1,55 @@
+#include "push_swap.h"
+
+t_stack	*lst_new(int nbr)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->data = nbr;
+	new->next = NULL;
+	return (new);
+}
+
+void	lst_addfront(t_stack **lst, t_stack *new)
+{
+	if (!new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+t_stack	*lst_last(t_stack *lst)
+{
+	t_stack	*last;
+
+	if (!lst)
+		return ;
+	last = lst;
+	while (last->next)
+		last = last->next;
+	return (last);
+}
+
+void	lst_addback(t_stack **lst, t_stack *new)
+{
+	t_stack *last;
+
+	if (!new || !lst)
+		return ;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	last = lst_last(*lst);
+	last->next = new;
+}
+
+//TO-DO => ----------------------------lst_new -/
+//TO-DO => ----------------------------lst_addfront -/
+//TO-DO => ----------------------------lst_addback -/
+//TO-DO => ----------------------------lst_last-/
+//TO-DO => ----------------------------lst_clear
+//TO-DO => ----------------------------lst_delone
